2acb9aace4b2e9da9a234fa6b8ee861906db2a29
Paulo <paulo@fazendadosoftware.com>
2022-01-03 09:28:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="height: calc(100vh - 7rem); padding: 1rem;">
+  <div style="padding: 1rem 0;">
+    iframe src:
+    <input v-model="src">
+  </div>
+
+    <iframe style="height: 100%; width: 100%" :src="src" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const src = ref('https://localhost:3000')
+</script>
